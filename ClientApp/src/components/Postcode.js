5205c361aef1308b974e418191c560b070b149dc
daria <dariamanskaia@hotmail.com>
2021-09-23 01:23:15 +0100

    starting with react

--- ClientApp/src/components/Postcode.js
+++ ClientApp/src/components/Postcode.js
@@ -1,91 +1,69 @@
 import React, { Component } from 'react';
 
-export class Postcode extends Component {
-  static displayName = Postcode.name;
+const validatePostcode = (postcode) => {
+  if (postcode.length <= 0){
+    return false;
+  }
+  return true;
+}
 
+export class Postcode extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {postcode: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    
+    validatePostcode("asdf");
+    console.log(event.target.value);
+    this.setState({postcode: event.target.value});
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    
+
+    const validation = validatePostcode(this.state.postcode);
+    if (!validation){
+      alert('Please submit a valid postcode');
+      return;
+    }
+    alert('Your postcode is: ' + this.state.postcode);
+
+    const url = 'https://api.postcodes.io/postcodes/' + this.state.postcode;
+    
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      }).then((data) => {
+        console.log(data)
+        //continuar a escrever
+      }); 
+
+
+  }
+  
   
 
 
   render() {
-  //   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    
-  //   const coordinatesAirport = {
-  //     latitude: 51.4700223,
-  //     longitude: -0.4542955
-  //   }
-  
-  //   const submitPostcodeClick = (e) => {
-  //     const postcode = $(".input_postcode").val();
-  //       if (!validatePostcode(postcode)){
-  //         alert("Please submit a valid postcode");
-  //         return;
-  //       }
-        
-  //       const url = 'hhtps://api.postcodes.io/postcodes/' + postcode;
-  //       $.get(url,
-  //         (data) => {
-  //           const coordinates = {
-  //             latitude: parseFloat(data.result.latitude),
-  //             longitude: parseFloat(data.result.longitude),
-  //           };
-
-  //           const distancekm = parseFloat(getDistanceFromLatLonInKm(coordinatesAirport.latitude, coordinatesAirport.longitude, coordinates.latitude, coordinates.longitude));
-
-  //           $('.distance_km').html(`This place is ${distanceKm.toFixed(2)}km away from the airport`);
-  //               $('distance_km').removeClass("hidden");
-  //         })
-  //         .fail(() => {
-  //           alert("Error. Postcode not found.");
-  //         })
-  //   };
-
-  //   // @param {string} postcode
-
-  //   const validatePostcode = (postcode) => {
-  //     if (postcode.length <= 0){
-  //       return false;
-  //     }
-  //     return true;
-  //   }
-
-  //   const deg2rad = (deg) => {
-  //     return deg * (Math.PI/180)
-  //   }
-  
-  // const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
-  //     var R = 6371; // Radius of the earth in km
-  //     var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  //     var dLon = deg2rad(lon2-lon1); 
-  //     var a = 
-  //       Math.sin(dLat/2) * Math.sin(dLat/2) +
-  //       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-  //       Math.sin(dLon/2) * Math.sin(dLon/2)
-  //       ; 
-  //     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  //     var d = R * c; // Distance in km
-  //     return d;
-  //   }
-
-      
-  // let km = ${'d'}
-  // let miles = km / 1.609;
-  // console.log(miles);
-  
-
-  // $(document).ready(() => {
-  //   $('.submit_postcode').click(submitPostcodeClick); //defining click event for submit button
-  // })
 
     return (
-      <div>
-        <h1>How far away are you from the airport?</h1>
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <h1>How far away are you from the airport?</h1>
 
-        <label for="input_postcode">Insert your postcode to calculate your distance to London Heathrow airport.</label>
-        <input name="input_postcode" type='text' class="input_postcode" placeholder='Insert postcode' />
-        <button class="submit_postcode">submit</button>
+          <label for="input_postcode">Insert your postcode to calculate your distance to London Heathrow airport.</label>
+          <input name="input_postcode" type='text' class="input_postcode" placeholder='Insert postcode' value={this.state.postcode} onChange={this.handleChange}/>
+          <input type="submit" class="submit_postcode" />
 
-        <div class="distance_km hidden"></div>
-      </div>
+          <div class="distance_km hidden"></div>
+        </div>
+      </form>
     );
   }
 }
