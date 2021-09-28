bcd3f43d8ac3d16a8bd6700f69885fa345ff4d68
daria <dariamanskaia@hotmail.com>
2021-09-29 00:49:03 +0100

    portfolio changes

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-grid-system';
+
+import image from '../img/anim.jpg';
+
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -6,23 +10,33 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Hello, Craftable Software Team!</h1>
+        <h1>Hey you!</h1>
         <p>Welcome to my humble web application, built with:</p>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
           <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
         </ul>
-        <p>To check out my solution to the techinal assignment you provided me please take a look at the <a href="/postcode">Postcode</a> route.</p>
+        <p>To check out my solution to the a techinal assignment made by me please take a look at the <a href="/postcode">Postcode</a> route.</p>
         
-        <p>Thank you for the opportunity to develop my skills as a full stack developer. Any doubt feel free to contact me.</p>
+        <p>I'm looking for an opportunity to develop my skills as a full stack developer. Any doubt feel free to contact me.</p>
         
-        <ul>
-          <p>Here you can find my Social pages:</p>
-          <li><a href='https://dariamanskaia.github.io/WorldOfDaria/'>Webpage</a></li>
-          <li><a href='https://github.com/dariamanskaia'>Github</a></li>
-          <li><a href='https://pt.linkedin.com/in/dariamanskaia'>Linkedin</a></li>
-        </ul>
+        <Container>
+          <Row>
+            <Col sm={6}>
+              <p>Here you can find my Social pages:</p>
+              <ul>
+                <li><a href='https://dariamanskaia.github.io/WorldOfDaria/'>Webpage</a></li>
+                <li><a href='https://github.com/dariamanskaia'>Github</a></li>
+                <li><a href='https://pt.linkedin.com/in/dariamanskaia'>Linkedin</a></li>
+                <li>Email - dariamanskaiag@gmail.com</li>
+              </ul>
+            </Col>
+            <Col sm={6}>
+              <img src={image} height={400} width={400} alt="AnimationImage"/>
+            </Col>
+          </Row>
+        </Container>
         
       </div>
     );
